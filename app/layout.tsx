721ba2042aff438ac50b12c1613d967713538b4f
alanmunoz <alanx015@hotmail.com>
2026-06-07 21:34:56 -0600

    missing save

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
     template: "%s · aDOGme",
   },
   description:
-    "Plataforma de adopción canina responsable en CDMX y área metropolitana " +
+    "Plataforma de adopción canina responsable en CDMX y área metropolitana." +
     "Conectamos adoptantes con refugios verificados para facilitar adopciones seguras.",
   keywords: [
     "adopción perros",
